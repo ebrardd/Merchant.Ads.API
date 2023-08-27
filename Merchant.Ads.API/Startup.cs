8352931bc41451e73a8a3ea28bfefb4bf7607c18
ebrardd <ebraremine.demircioglu@st.uskudar.edu.tr>
2023-08-28 01:32:32 +0300

    merchantcontroller

--- Merchant.Ads.API/Startup.cs
+++ Merchant.Ads.API/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
 using Merchant.Ads.API.Configs;
+using Merchant.Ads.API.Extensions;
+using Merchant.Ads.API.Extensions.CustomExceptionMiddleWare;
+using Merchant.Ads.API.Helpers;
 using Merchant.Ads.API.Repositories;
 using Merchant.Ads.API.Services;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+
 
 namespace Merchant.Ads.API
 {
@@ -22,14 +26,15 @@ namespace Merchant.Ads.API
             .SetBasePath(env.ContentRootPath)
             .AddJsonFile("Configs/appsettings.json")
             .AddJsonFile("Configs/appsettings.Development.json")
-            .AddEnvironmentVariables();     
+            .AddEnvironmentVariables();
             Configuration = builder.Build();
+
             var envVariables = Environment.GetEnvironmentVariables();
             if (string.IsNullOrWhiteSpace(envVariables["ASPNETCORE_ENVIRONMENT"]?.ToString()))
                 throw new ArgumentNullException("ASPNETCORE_ENVIRONMENT");
             _environment = envVariables["ASPNETCORE_ENVIRONMENT"].ToString();
             _settings = Configuration.GetSection("Settings").Get<Settings>();
-            _mongoDbSettings = Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+            // _mongoDbSettings = Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
         }
 
         public IConfiguration Configuration { get; }
@@ -37,52 +42,74 @@ namespace Merchant.Ads.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddCors(options => // Cross Origin Source
             {
                 options.AddPolicy("AllowAll", builder => {
                     builder
-               // .WithOrigins("*")
+                // .WithOrigins("*")
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials();
                 });
             });
-            services.Configure<MongoDBSettings>(Configuration.GetSection("MongoDBSettings"));
+
+
             /*services.AddFluentValidationAutoValidation();
             services.AddFluentValidationClientsideAdapters();
             services.AddValidatorsFromAssemblyContaining<MerchantValidator>(); */
-            services.AddControllers()
-                    .AddFluentValidation(options =>
-             {
-                 options.ImplicitlyValidateChildProperties = true;
-                 options.ImplicitlyValidateRootCollectionElements = true;
-                 options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-             });
+            services.AddControllers();
+            services.AddHttpClient();
+            services.AddMvc();
+
+
+            /*    .AddFluentValidation(options =>
+         {
+             options.ImplicitlyValidateChildProperties = true;
+             options.ImplicitlyValidateRootCollectionElements = true;
+             options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+         });*/
+
+            services.AddSingleton<ILoggerManager, LoggerManager>();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
             services.AddSingleton(_settings);
             services.AddResponseCompression();
             services.AddSingleton<IService, Service>();
             services.AddSingleton<IMerchantRepository, MerchantRepository>();
-            services.AddSingleton(_mongoDbSettings);      
+            services.AddSingleton<MongoDBSettings>();
+            services.Configure<MongoDBSettings>(Configuration.GetSection("MongoDBSettings"));
+
+            //services.AddSingleton( _mongoDbSettings);
+
+
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)//ILoggerManager,Logger
         {
+            app.UseMiddleware<ExceptionMiddleWare>();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.ConfigureExceptionHandler(logger);
+            // app.UseMiddleware<ExceptionMiddleWare>();
 
+            // app.UseHttpsRedirection();
+
+
+            //app.UseAuthentication();    
             app.UseCors("AllowAll");
             app.UseResponseCompression();
             app.UseRouting();
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+
         }
     }
-}
+} 
 
 
 
--- Merchant.Ads.API/V1/Controllers/MerchantController.cs
+++ Merchant.Ads.API/V1/Controllers/MerchantController.cs
@@ -2,6 +2,16 @@ using Merchant.Ads.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Merchant.Ads.API.Services;
 using System.Net;
+using Merchant.Ads.API.Extensions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Merchant.Ads.API.Repositories;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Merchant.Ads.API.V1.Models.ResponseModels;
+using Merchant.Ads.API.V1.Models.RequestModels;
+
 
 namespace Merchant.Ads.API.V1.Controllers
 {
@@ -10,12 +20,17 @@ namespace Merchant.Ads.API.V1.Controllers
     public class MerchantController : ControllerBase
     {
         private readonly IService _merchantService;
-        public MerchantController(IService service)
+        private readonly ILogger<MerchantController> _logger;
+
+        public MerchantController(IService service, ILogger<MerchantController>logger)
         {
             _merchantService = service;
+            _logger = logger;
+            
+
         }
         [HttpPost]
-        public IActionResult Create([FromBody] MerchantModel merchant)
+        public IActionResult Create([FromBody] MerchantCreateRequestModel merchant)//REQAL
         {
             _merchantService.Create(merchant);
             return CreatedAtAction(nameof(Create), merchant);//aksiyonuolusturdun
@@ -26,6 +41,149 @@ namespace Merchant.Ads.API.V1.Controllers
             var merchant = _merchantService.GetById(Id);
             return Ok(merchant);
         }
-       
+        [HttpPost("PostAsync")]
+        public async Task<IActionResult> PostAsync([FromBody] MerchantModel merchantModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            MerchantModel insertedMerchant = await _merchantService.InsertMerchantAsync(merchantModel);
+
+            return Ok(insertedMerchant);
+        }
+
+        [HttpPut("{ID}")]
+        public async Task<ActionResult<MerchantModel>> Update(int id, MerchantModel merchantModel)
+        {
+            try
+            {
+                if (id != merchantModel.Id)
+                    return BadRequest("MerchantModel Id mismatch");
+
+                var merchantModelToUpdate = await _merchantService.Get(id, merchantModel);
+
+                if (merchantModelToUpdate is null)
+                    return NotFound($"MerchantModel with Id = {id} not found");
+
+                var updatedMerchantModel = await _merchantService.Update(id, merchantModelToUpdate);
+
+                if (updatedMerchantModel != null)
+                {
+                    return Ok(updatedMerchantModel);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Error updating data");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating data");
+            }
+        
+        
+
+       /* [HttpPatch("{id}")]
+        async Task<IActionResult> PartiallyUpdateMerchantForCompany(int TaxNo, int id, [FromBody] JsonPatchDocument<MerchantModel> patchDoc)
+                                                                                        
+        {
+            if (patchDoc == null)
+            {
+                _logger.LogError("patchDoc object sent from client is null.");
+                return BadRequest("patchDoc object is null");
+            }
+
+            var company = await _merchantService.Company.Get(TaxNo, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInformation($"Company with id: {TaxNo} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var merchantModelEntity = await _merchantService.MerchantModel.Get(TaxNo, id, trackChanges: true);
+            if (merchantModelEntity == null)
+            {
+                _logger.LogInformation($"MerchantModel with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+                var merchantModelToPatch = _mapper.Map<MerchantModel>(merchantModelEntity);
+
+                patchDoc.ApplyTo(merchantModelToPatch);
+
+                _mapper.Map(merchantModelToPatch, merchantModelEntity);
+
+                await _merchantService.Save();
+
+                return NoContent();
+            } */
+    }
+
+
+    [HttpDelete("{ID}")]
+            
+            public async Task<ActionResult<MerchantModel>> Delete(int id,MerchantModel merchantModel)
+            {
+                try
+                {
+                    var merchantModelToDelete = await _merchantService.Get(id,merchantModel);
+
+                    if (merchantModelToDelete == null)
+                    {
+                        return NotFound($"Employee with Id = {id} not found");
+                    }
+
+                    var deletedMerchantModel = await _merchantService.DeleteMerchant(id,merchantModel);
+
+                    if (deletedMerchantModel != null)
+                    {
+                        return Ok(deletedMerchantModel); 
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError,
+                            "Error deleting data");
+                    }
+                }
+                catch (Exception)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Error deleting data");
+                }
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+
+    internal class Update
+    {
+    }
+
+    public class JsonPatchDocument<T>
+    {
+        internal void ApplyTo(object merchantModelToPatch)
+        {
+            throw new NotImplementedException();
         }
     }
+}
+
+
+
+
+
+    
+
+
+
+
+
